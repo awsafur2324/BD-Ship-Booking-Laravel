4c319fbbf027cd6fbee52cdb4ffb59ce9ebf6f4b
SUHRID KHAN <awsafur15-2324@diu.edu.bd>
2024-12-15 07:13:41 +0600

    footer upate

--- resources/views/components/app/footer.blade.php
+++ resources/views/components/app/footer.blade.php
@@ -59,7 +59,7 @@
         <div class="w-full md:w-4/12 px-4 mx-auto text-center">
           <div class="text-sm text-blueGray-500 font-semibold py-1">
             Copyright © <span id="get-current-year">{{date('Y')}}</span><a href="#" class="text-blueGray-500 hover:text-gray-800" target="_blank"> BD Ship Booking </a> by
-            <a href="#" class="text-blueGray-500 hover:text-blueGray-800">Awsafur</a>.
+            <a href="#" class="text-blueGray-500 hover:text-blueGray-800">Shihab Hasan</a>.
           </div>
         </div>
       </div>
