e74e54355b3ab0b0dd372a7335cbd9d75ab81e8f
SUHRID KHAN <awsafur15-2324@diu.edu.bd>
2025-03-10 03:57:45 +0600

    index.php , hatccess , AppServiceProvide updated

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-
+$_SERVER['DOCUMENT_ROOT'] = __DIR__ . '/public';
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
